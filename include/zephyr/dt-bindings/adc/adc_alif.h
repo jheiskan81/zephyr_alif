5f89e79bd8339963c05ffa530754d1c41bee778c
Prabhakar Kumar <prabhakar.kumar@alifsemi.com>
2025-09-11 12:29:18 +0530

    drivers: adc: add support for alif adc12/24
    Add ADC12/24 (Analog to digital conversion) driver.
    
    Signed-off-by: Prabhakar Kumar <prabhakar.kumar@alifsemi.com>

---
+++ include/zephyr/dt-bindings/adc/adc_alif.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2025 Alif Semiconductor.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_ADC_ALIF_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_ADC_ALIF_H_
+
+#include <zephyr/dt-bindings/dt-util.h>
+
+#define ADC_DONE0_INTERRUPT     BIT(0)
+#define ADC_DONE1_INTERRUPT     BIT(1)
+#define ADC_COMPA_INTERRUPT     BIT(2)
+#define ADC_COMPB_INTERRUPT     BIT(3)
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_ADC_ALIF_H_ */
