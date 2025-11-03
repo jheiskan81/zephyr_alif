77eebee5e6bedc24f312d1141ef15e34ccba5096
Silesh C V <silesh@alifsemi.com>
2025-11-03 17:53:25 +0530

    soc: alif: ensemble: e8: add support for APSS
    Add support for the cortex-a32 application processor residing
    in the application processor subsytem (APSS) of the ensemble
    e8 SOC.
    
    Signed-off-by: Silesh C V <silesh@alifsemi.com>

--- soc/alif/ensemble/e8/soc.h
+++ soc/alif/ensemble/e8/soc.h
@@ -6,6 +6,8 @@
 #ifndef _SOC_H_
 #define _SOC_H_
 
+#if defined(CONFIG_RTSS_HE) || defined(CONFIG_RTSS_HP)
 #include <cmsis_core_m_defaults.h>
+#endif /*CONFIG_RTSS_HE || CONFIG_RTSS_HP*/
 
 #endif /* _SOC_H_ */
