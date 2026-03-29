663414637c453da92be18d4fb1690e3ce4c453e4
Sudhir Sreedharan <sudhir@alifsemi.com>
2026-03-29 21:05:28 +0530

    dts: alif: dma: add clock specifier and remove soc-level clock enable
    Add clocks property to all Alif DMA DT nodes using the appropriate
    clock specifier (ALIF_DMA0/1/2_CLK) for each SoC variant.
    
    Remove the DMA clock enable from soc files; it is now
    handled by the pl330 driver via clock_control_on().
    
    Signed-off-by: Sudhir Sreedharan <sudhir@alifsemi.com>

--- soc/alif/balletto/common/soc_common.c
+++ soc/alif/balletto/common/soc_common.c
@@ -118,7 +118,6 @@ static int soc_init(void)
 
 	/* Enable DMA */
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma2), arm_dma_pl330, okay)
-	sys_set_bits(M55HE_CFG_HE_CLK_ENA, BIT(4));
 	sys_clear_bits(M55HE_CFG_HE_DMA_CTRL, BIT(0));
 	sys_write32(0U, M55HE_CFG_HE_DMA_IRQ);
 	sys_write32(0U, M55HE_CFG_HE_DMA_PERIPH);
--- soc/alif/ensemble/common/soc_common.c
+++ soc/alif/ensemble/common/soc_common.c
@@ -201,7 +201,6 @@ static int soc_init(void)
 
 	/* Enable DMA */
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma0), arm_dma_pl330, okay) /* dma0 */
-	sys_set_bits(CLKCTRL_PER_MST_PERIPH_CLK_EN, BIT(4));
 	sys_clear_bits(CLKCTRL_PER_MST_DMA_CTRL, BIT(0));
 	sys_write32(0U, CLKCTRL_PER_MST_DMA_IRQ);
 	sys_write32(0U, CLKCTRL_PER_MST_DMA_PERIPH);
@@ -209,7 +208,6 @@ static int soc_init(void)
 #endif
 
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma1), arm_dma_pl330, okay) /* dma1 */
-	sys_set_bits(M55HP_CFG_HP_CLK_ENA, BIT(4));
 	sys_clear_bits(M55HP_CFG_HP_DMA_CTRL, BIT(0));
 	sys_write32(0U, M55HP_CFG_HP_DMA_IRQ);
 	sys_write32(0U, M55HP_CFG_HP_DMA_PERIPH);
@@ -217,7 +215,6 @@ static int soc_init(void)
 #endif
 
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma2), arm_dma_pl330, okay) /* dma2 */
-	sys_set_bits(M55HE_CFG_HE_CLK_ENA, BIT(4));
 	sys_clear_bits(M55HE_CFG_HE_DMA_CTRL, BIT(0));
 	sys_write32(0U, M55HE_CFG_HE_DMA_IRQ);
 	sys_write32(0U, M55HE_CFG_HE_DMA_PERIPH);
